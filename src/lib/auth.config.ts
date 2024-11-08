2819c652c616dae26616190a7d236514ecb468c0
aBgAmeuR <a.josset@yahoo.com>
2024-11-08 22:10:07 +0100

    fix: harmoniser les guillemets dans les fichiers de configuration et de middleware

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -1,11 +1,11 @@
-import type { NextAuthConfig } from "next-auth";
-import Spotify from "next-auth/providers/spotify";
+import type { NextAuthConfig } from 'next-auth';
+import Spotify from 'next-auth/providers/spotify';
 
 export default {
   providers: [
     Spotify({
       authorization:
-        "https://accounts.spotify.com/authorize?scope=user-read-recently-played%20user-top-read",
+        'https://accounts.spotify.com/authorize?scope=user-read-recently-played%20user-top-read'
     })
   ],
   callbacks: {
