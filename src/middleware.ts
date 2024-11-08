21b73bc192994c41636da729e10e4ca71377dcc1
aBgAmeuR <a.josset@yahoo.com>
2024-11-08 22:12:58 +0100

    fix: update authorization URL and enhance authentication middleware logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,21 @@
-import NextAuth from 'next-auth';
+import NextAuth from "next-auth";
 
-import authConfig from '@/lib/auth.config';
+import authConfig from "@/lib/auth.config";
 
 const { auth } = NextAuth(authConfig);
 export default auth(async (req) => {
   if (
     !req.auth &&
-    req.nextUrl.pathname !== '/' &&
-    !req.nextUrl.pathname.startsWith('/api/auth')
+    req.nextUrl.pathname !== "/" &&
+    !req.nextUrl.pathname.startsWith("/api/auth")
   ) {
-    const newUrl = new URL('/api/login', req.nextUrl.origin);
-    newUrl.searchParams.set('callbackUrl', req.nextUrl.pathname);
+    const newUrl = new URL("/api/login", req.nextUrl.origin);
+    newUrl.searchParams.set("callbackUrl", req.nextUrl.pathname);
 
     return Response.redirect(newUrl);
   }
 });
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
 };
