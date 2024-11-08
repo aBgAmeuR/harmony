b5f735411b542a698786dd29165fb13b1b463c6d
aBgAmeuR <a.josset@yahoo.com>
2024-11-08 23:02:31 +0100

    fix: update authorization URL and enhance authentication middleware logic

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -1,16 +1,11 @@
 import type { NextAuthConfig } from "next-auth";
 import Spotify from "next-auth/providers/spotify";
 
-const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
-const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
-
 export default {
   providers: [
     Spotify({
       authorization:
-        "https://accounts.spotify.com/authorize?scope=user-read-email%20user-read-recently-played%20user-top-read",
-      clientId: SPOTIFY_CLIENT_ID,
-      clientSecret: SPOTIFY_CLIENT_SECRET
+        "https://accounts.spotify.com/authorize?scope=user-read-recently-played%20user-top-read"
     })
   ],
   callbacks: {
