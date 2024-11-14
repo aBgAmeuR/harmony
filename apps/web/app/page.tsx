028c0b54665692491671a5feb61d0d304b3fab12
Antoine JOSSET <a.josset@yahoo.com>
2024-11-14 19:34:48 +0100

    feat: implement authentication package with NextAuth and Spotify integration

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,17 @@
+import { auth } from "@repo/auth";
 import { prisma } from "@repo/database";
 import { Button } from "@repo/ui/components/ui/button";
 
 export default async function IndexPage() {
   const users = await prisma.user.findMany();
+  const session = await auth();
 
   return (
     <div>
       <p className="text-4xl font-bold">Hello World</p>
       <pre>{JSON.stringify(users, null, 2)}</pre>
       <Button variant="default">Click me</Button>
+      <pre>{JSON.stringify(session, null, 2)}</pre>
     </div>
   );
 }
---
+++ packages/auth/src/index.ts
@@ -0,0 +1,5 @@
+import { handlers, signIn, signOut, auth } from "./auth";
+
+import authConfig from "./auth.config";
+
+export { handlers, signIn, signOut, auth, authConfig };
