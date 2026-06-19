fc65c1f7dfae93e55f4e17f3c71b9bf862457d91
Antoine <a.josset@yahoo.com>
2026-06-19 21:17:06 +0000

    chore: remove deprecated API files and configurations

--- apps/api/otel.ts
+++
@@ -1,9 +0,0 @@
-/**
- * OpenTelemetry initialization file.
- *
- * IMPORTANT: This file must be imported FIRST in bin/server.ts
- * for auto-instrumentation to work correctly.
- */
-import { init } from '@adonisjs/otel/init'
-
-await init(import.meta.dirname)
